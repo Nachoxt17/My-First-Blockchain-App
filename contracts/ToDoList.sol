1137351c99809fe4dab069d30aed5291b77ba525
Nachoxt17 <ignacioceaglio@gmail.com>
2021-01-11 16:17:47 -0300

    8th Commit

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -13,15 +13,19 @@ contract ToDoList {
 /**+-Mapping in Solidity:_ https://www.geeksforgeeks.org/solidity-mappings/ */
   mapping(uint => Task) public tasks;
 
+event TaskCreated(
+  uint id,
+  string content,
+  bool completed
+);
+
 constructor() public {
-  createTask("Drink 2 litters of water every day.");
+  createTask('Drink 2 litters of water every day.');
 }
 
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
-  }
-
 /**+-I will explain this function:_
 
 1-First, we create the function with the function keyword, and give it a name createTask()
@@ -32,4 +36,8 @@ We specify that this argument will be of "string" data type, and that it will pe
 4-Inside the function, we create an "id" for the new task. We simply take the existing taskCount and increment it by 1.
 5-Now we create a new task "Document" based on the "struct" by calling Task(taskCount, _content, false); and passing in the values for the new task.
 6-Next, we store the new task on the Blockchain by adding it to the tasks mapping like this: task[taskCount] = ..... .*/
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+
 }
